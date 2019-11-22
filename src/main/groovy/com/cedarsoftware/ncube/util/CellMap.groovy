048cc67d77a8005dbd2f389e9ca4306e2b40acb4
John DeRegnaucourt <jdereg@gmail.com>
2019-11-22 10:12:15 -0500

    - added missing copyright.

--- src/main/groovy/com/cedarsoftware/ncube/util/CellMap.groovy
+++ src/main/groovy/com/cedarsoftware/ncube/util/CellMap.groovy
@@ -3,6 +3,27 @@ package com.cedarsoftware.ncube.util
 import gnu.trove.THashMap
 import groovy.transform.CompileStatic
 
+/**
+ * Holds all of the cells values of an NCube.  The keys of the Map are a Set<Long>.  This Set
+ * contains all of the column IDs that this cell points to.  The value-side of the map is the
+ * actual cell value (a long, a String, a GroovyExpression, etc.)
+ *
+ * @author John DeRegnaucourt (jdereg@gmail.com)
+ *         <br>
+ *         Copyright (c) Cedar Software LLC
+ *         <br><br>
+ *         Licensed under the Apache License, Version 2.0 (the "License")
+ *         you may not use this file except in compliance with the License.
+ *         You may obtain a copy of the License at
+ *         <br><br>
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *         <br><br>
+ *         Unless required by applicable law or agreed to in writing, software
+ *         distributed under the License is distributed on an "AS IS" BASIS,
+ *         WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *         See the License for the specific language governing permissions and
+ *         limitations under the License.
+ */
 @CompileStatic
 class CellMap<V> extends THashMap<Set<Long>, V>
 {
